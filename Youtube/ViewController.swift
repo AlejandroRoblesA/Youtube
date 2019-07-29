8d94b616c99e93d9a0b45531b26e7bca2b21ed1d
Alejandro <alejandro.rob.alt@gmail.com>
2019-07-28 21:31:49 -0500

    Files to start the project created
    The files were created by xcode

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Alejandro on 7/28/19.
+//  Copyright © 2019 Alejandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
