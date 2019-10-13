a2ccbf804d1355e9016ed757eedeaf6a5e3e71fb
Alejandro <alejandro.rob.alt@gmail.com>
2019-10-12 20:03:11 -0500

    Setting view customized

--- Youtube/Controller/SettingsLauncher.swift
+++ Youtube/Controller/SettingsLauncher.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
-class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDelegate {
+class Setting: NSObject {
+    let name: String
+    let imageName: String
+    
+    init(name: String, imageName: String) {
+        self.name = name
+        self.imageName = imageName
+    }
+}
+
+class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     let blackView = UIView()
-    
+    let cellHeight: CGFloat = 50
     let collectionView: UICollectionView = {
         
         let layout = UICollectionViewFlowLayout()
@@ -20,8 +30,19 @@ class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDe
         return cv
     }()
     
+    let settings: [Setting] = {
+        return [Setting(name: "Settings", imageName: "settings"),
+                Setting(name: "Terms & privacy policy", imageName: "privacy"),
+                Setting(name: "Send Feedback", imageName: "feedback"),
+                Setting(name: "Help", imageName: "help"),
+                Setting(name: "Switch Account", imageName: "switch_account"),
+                Setting(name: "Cancel", imageName: "cancel")]
+    }()
+    
     
     let cellId = "cellId"
+    
+    
     
     @objc func showSettings(){
         
@@ -31,7 +52,7 @@ class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDe
             blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
             window.addSubview(blackView)
             window.addSubview(collectionView)
-            let height: CGFloat = 200
+            let height: CGFloat = CGFloat(settings.count) * cellHeight
             let y = window.frame.height - height
             collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
             blackView.frame = window.frame
@@ -55,13 +76,24 @@ class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDe
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return settings.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SettingCell
+        let setting = settings[indexPath.item]
+        cell.setting = setting
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
     
     override init() {
         super.init()
--- Youtube/View/SettingCell.swift
+++ Youtube/View/SettingCell.swift
@@ -9,9 +9,53 @@
 import UIKit
 
 class SettingCell: BaseCell {
+    
+    override var isHighlighted: Bool{
+        didSet{
+            backgroundColor = isHighlighted ? UIColor.darkGray : UIColor.white
+            nameLabel.textColor = isHighlighted ? UIColor.white : UIColor.black
+            iconImageView.tintColor = isHighlighted ? UIColor.white : UIColor.darkGray
+        }
+    }
+    
+    var setting: Setting?{
+        didSet{
+            nameLabel.text = setting?.name
+            if let imageName = setting?.imageName{
+                iconImageView.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
+                iconImageView.tintColor = .darkGray
+            }
+        }
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Setting"
+        label.font = UIFont.systemFont(ofSize: 13)
+        return label
+    }()
+    
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "settings")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    
+    
     override func setupViews() {
         super.setupViews()
         
-        backgroundColor = .red
+        addSubview(nameLabel)
+        addSubview(iconImageView)
+        
+        
+        
+        addConstraintsWithFornat(format: "H:|-8-[v0(30)]-8-[v1]|", views: iconImageView, nameLabel)
+        addConstraintsWithFornat(format: "V:|[v0]|", views: nameLabel)
+        addConstraintsWithFornat(format: "V:|[v0(30)]|", views: iconImageView)
+        
+        addConstraint(NSLayoutConstraint(item: iconImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
     }
 }
