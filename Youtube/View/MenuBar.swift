b159e7ca0cb02b6dd7060c39b830b8c154a1ad60
Alejandro <alejandro.rob.alt@gmail.com>
2019-10-14 21:27:38 -0500

    Horizontal bar view animated

--- Youtube/View/MenuBar.swift
+++ Youtube/View/MenuBar.swift
@@ -57,6 +57,11 @@ class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIC
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let x = CGFloat(indexPath.item) * frame.width/4
         horizontalBarLeftAnchorConstraint?.constant = x
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.layoutIfNeeded()
+        }, completion: nil)
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
