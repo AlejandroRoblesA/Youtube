9cbcdc3779196fce2e8938a01d4bf656f20bb725
Alejandro <alejandro.rob.alt@gmail.com>
2019-10-18 17:50:14 -0500

    Section trending showed

--- Youtube/View/TrendingCell.swift
+++ Youtube/View/TrendingCell.swift
@@ -11,7 +11,10 @@ import UIKit
 class TrendingCell: FeedCell {
     
     override func fetchVideos() {
-        
+        ApiService.sharedInstance.fetchTrendingFeed { (videos) in
+            self.videos = videos
+            self.collectionView.reloadData()
+        }
     }
     
 }
