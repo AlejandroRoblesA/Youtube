94d5df377d8952191451f5b6c2782077d3b133f8
Alejandro <alejandro.rob.alt@gmail.com>
2019-10-20 12:59:43 -0500

    KeyWindow as view setted

--- Youtube/Helpers/VideoLauncher.swift
+++ Youtube/Helpers/VideoLauncher.swift
@@ -12,6 +12,20 @@ class VideoLauncher: NSObject{
     
     func showVideoPlayer(){
         print("Animation")
+        
+        if let keyWindow = UIApplication.shared.keyWindow{
+            let view = UIView(frame: keyWindow.frame)
+            view.backgroundColor = .white
+            view.frame = CGRect(x: keyWindow.frame.width - 10, y: keyWindow.frame.height - 10, width: 10, height: 10)
+            
+            keyWindow.addSubview(view)
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                view.frame = keyWindow.frame
+            }) { (completedAnimation) in
+                //some code
+            }
+        }
     }
     
 }
