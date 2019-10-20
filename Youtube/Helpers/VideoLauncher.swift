5dd0230405dc5485242a146f3148f1fcb96feb0c
Alejandro <alejandro.rob.alt@gmail.com>
2019-10-20 12:47:50 -0500

    Class VideoLauncher

---
+++ Youtube/Helpers/VideoLauncher.swift
@@ -0,0 +1,17 @@
+//
+//  VideoLauncher.swift
+//  Youtube
+//
+//  Created by Alejandro on 20/10/19.
+//  Copyright © 2019 Alejandro. All rights reserved.
+//
+
+import UIKit
+
+class VideoLauncher: NSObject{
+    
+    func showVideoPlayer(){
+        print("Animation")
+    }
+    
+}
